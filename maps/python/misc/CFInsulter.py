d1c1fea83cfa5a66acb5cdf68a5cdb685900dc63
ephraim baker <ephraim.baker@yahoo.com>
2019-04-16 15:33:55 -0400

    skull test

--- maps/python/misc/CFInsulter.py
+++ maps/python/misc/CFInsulter.py
@@ -22,25 +22,7 @@ def Insult(style):
     adj2 = ['elf-footed', 'dull-axed', 'tin-shielded', 'horse-knappin', 'hog-lickin', 'knock-kneed', 'jug-eared', 'pie-headed', 'snaggle-toothed', 'brown-nosed', 'lilly-livered', 'base-court', 'bat-fowling', 'beef-witted', 'beetle-headed', 'boil-brained', 'clapper-clawed','clay-brained', 'common-kissing', 'crook-pated', 'dismal-dreaming', 'dizzy-eyed', 'doghearted', 'dread-bolted', 'earth-vexing', 'elf-skinned', 'fat-kidneyed', 'fen-sucked', 'flap-mouthed', 'fly-bitten', 'folly-fallen', 'fool-born', 'full-gorged', 'guts-griping', 'half-faced', 'hasty-witted', 'hedge-born', 'hell-hated', 'idle-headed', 'ill-breeding', 'ill-nurtured', 'knotty-pated', 'milk-livered', 'motley-minded', 'onion-eyed', 'plume-plucked', 'pottle-deep', 'pox-marked', 'reeling-ripe', 'rough-hewn', 'rude-growing', 'rump-fed', 'shard-borne', 'sheep-biting', 'spur-galled', 'swag-bellied', 'tardy-gaited', 'tickle-brained', 'toad-spotted', 'unchin-snouted', 'weather-bitten', 'weather-beaten', 'mutton-eating', 'coffee-nosed', 'malodorous']
     noun = ['orc-lover', 'gobiln-kisser', 'tree-embracer', 'dipshit', 'city-slicker', 'root-head', 'cow-pie', 'greenhorn', 'idgit', 'apple-john', 'baggage', 'barnacle', 'bladder', 'boar-pig', 'bugbear', 'bum-bailey', 'canker-blossom', 'clack-dish', 'clotpole', 'coxcomb', 'codpiece', 'death-token', 'dewberry', 'flap-dragon', 'flax-wench', 'flirt-gill', 'foot-licker', 'fustilarian', 'giglet', 'gudgeon', 'haggard', 'harpy', 'hedge-pig', 'horn-beast', 'hugger-mugger', 'joithead', 'lewdster', 'lout', 'maggot-pie', 'malt-worm', 'mammet', 'measle', 'minnow', 'miscreant', 'moldwarp', 'mumble-news', 'nut-hook', 'pigeon-egg', 'pignut', 'puttock', 'pumpion', 'ratsbane', 'scut', 'skainsmate', 'strumpet', 'varlet', 'vassal', 'whey-face', 'wagtail', 'phlegm-barrel', 'numb-skull', 'lip-infection', 'blood-clot', 'boar-tick', 'pervert']
     prefixA = ['You are a','Your Matron\'s a', 'You', 'Yer a','Yer Mama\'s a', 'Yew', 'Thou art a','Thy Mother is a', 'Thou']
-    prefixAn = [['You are an', 'Your Matron\'s an', 'You', 'Yer an', 'Yer Mama\'s an', 'Yew', 'Thou art an', 'Thy Mother is an', 'Thou']
-
-##TEXAS STYLE
-
-  elif style == "texas":
-    adj1 = []
-    adj2 = [ ]
-    noun = []
-    prefixA = []
-    prefixAn = []
-
-##DWARVEN STYLE
-
-  elif style == "dwarf":
-    adj1 = []
-    adj2 = [ ]
-    noun = []
-    prefixA = []
-    prefixAn = ]
+    prefixAn = ['You are an', 'Your Matron\'s an', 'You', 'Yer an', 'Yer Mama\'s an', 'Yew', 'Thou art an', 'Thy Mother is an', 'Thou']
 
 ##NO STYLE
   else:

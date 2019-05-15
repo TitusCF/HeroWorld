744200b59c584ab0fc8bed4d97742c6d5540fac0
ephraim baker <ephraim.baker@yahoo.com>
2019-05-14 20:43:15 -0400

    IPO dialog update

--- maps/python/IPO/say.py
+++ maps/python/IPO/say.py
@@ -66,7 +66,10 @@ if text[0] == 'help' or text[0] == 'yes':
 	whoami.Say('- pen (%s platinum)\n- literacy scroll (%s platinum)'%(priceWritingPen,priceScrollOfLiteracy))
 	whoami.Say('You can send letters to friends:\n')
 	whoami.Say('- mailscroll <friend> (%s platinum)'%(priceMailScroll))
-	whoami.Say('You can also send items, say package followed by the name of the player. Cost is calculated by weight')
+	whoami.Say('You can also send items:')
+	whoami.Say('bag <friend>: 5 platinum')
+	whoami.Say('package <friend>: 50 platinum')
+	whoami.Say('carton <friend>: 200 platinum')
 	for pack in packages.keys():
 		# weight is in grams, so need to convert.
 		whoami.Say('- %s (max weight: %d kg, price: %d platinum)'%(pack, packages[pack][1] / 1000, packages[pack][0]))

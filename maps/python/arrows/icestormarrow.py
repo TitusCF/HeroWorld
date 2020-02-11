348f8a192974e0d45c34b3931eb53d5550e0afdf
TitusCF <46248632+TitusCF@users.noreply.github.com>
2020-02-11 10:10:24 -0500

    couple new arrows

---
+++ maps/python/arrows/icestormarrow.py
@@ -0,0 +1,13 @@
+import Crossfire
+spellob = Crossfire.CreateObjectByName("spell_large_icestorm")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+spellob = Crossfire.CreateObjectByName("spell_large_icestorm")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+spellob = Crossfire.CreateObjectByName("spell_large_icestorm")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+
+
+
---
+++ maps/python/arrows/vitriolarrow.py
@@ -0,0 +1,13 @@
+import Crossfire
+spellob = Crossfire.CreateObjectByName("spell_vitriol_splash")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+spellob = Crossfire.CreateObjectByName("spell_vitriol_splash")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+spellob = Crossfire.CreateObjectByName("spell_vitriol_splash")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+
+
+

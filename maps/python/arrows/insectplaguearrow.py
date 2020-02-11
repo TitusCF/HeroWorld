44b032edcb7d1ae1c597d3c935a3e3492542457f
TitusCF <46248632+TitusCF@users.noreply.github.com>
2020-02-11 10:33:12 -0500

    insect swarm arrow

--- maps/python/arrows/insectplaguearrow.py
+++ maps/python/arrows/insectplaguearrow.py
@@ -1,11 +1,11 @@
 import Crossfire
-spellob = Crossfire.CreateObjectByName("spell_create_earth_wall")
+spellob = Crossfire.CreateObjectByName("spell_insect_plague")
 Crossfire.WhoAmI().CastAbility(spellob, 0, "")
 spellob.Remove()
-spellob = Crossfire.CreateObjectByName("spell_create_earth_wall")
+spellob = Crossfire.CreateObjectByName("spell_insect_plague")
 Crossfire.WhoAmI().CastAbility(spellob, 0, "")
 spellob.Remove()
-spellob = Crossfire.CreateObjectByName("spell_create_earth_wall")
+spellob = Crossfire.CreateObjectByName("spell_insect_plague")
 Crossfire.WhoAmI().CastAbility(spellob, 0, "")
 spellob.Remove()
 

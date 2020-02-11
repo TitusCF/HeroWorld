dbd69e4992944e2ca0fec83cd8017341d0d1a0a9
TitusCF <46248632+TitusCF@users.noreply.github.com>
2020-02-11 12:43:43 -0500

    arrow

---
+++ maps/python/arrows/paralyzebombarrow.py
@@ -0,0 +1,25 @@
+import Crossfire
+spellob = Crossfire.CreateObjectByName("spell_create_bomb")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+spellob = Crossfire.CreateObjectByName("spell_create_bomb")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+spellob = Crossfire.CreateObjectByName("spell_create_bomb")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+spellob = Crossfire.CreateObjectByName("spell_create_bomb")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+spellob = Crossfire.CreateObjectByName("spell_paralyze")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+spellob = Crossfire.CreateObjectByName("spell_paralyze")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+
+
+
+
+
+

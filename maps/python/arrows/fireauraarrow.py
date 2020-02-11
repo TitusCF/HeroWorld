88729bbcced133fa35a7a36f128d06c5177745ee
TitusCF <46248632+TitusCF@users.noreply.github.com>
2020-02-11 09:51:49 -0500

    orb arrow

--- maps/python/arrows/fireauraarrow.py
+++ maps/python/arrows/fireauraarrow.py
@@ -1,15 +1,15 @@
 import Crossfire
 #Rednaxela helped me alot with this, gave me all the code --MikeeUSA--
 #Fire burning hands in all directions (splashback).
-spellob = Crossfire.CreateObjectByName("spell_burning_hands")
+spellob = Crossfire.CreateObjectByName("spell_holy_orb")
 Crossfire.WhoAmI().CastAbility(spellob, 0, "")
 spellob.Remove()
 #Fire burning hands "forward" (whatever direction the arrow is going in).
-#spellob = Crossfire.CreateObjectByName("spell_burning_hands")
+#spellob = Crossfire.CreateObjectByName("spell_holy_orb")
 #Crossfire.WhoAmI().CastAbility(spellob, Crossfire.WhoAmI().Direction, "")
 #spellob.Remove()
 #Cast create bomb spell (Igniting the bomb at the end of the arrow)
-spellob = Crossfire.CreateObjectByName("spell_flaming_aura")
+spellob = Crossfire.CreateObjectByName("spell_holy_orb")
 Crossfire.WhoAmI().CastAbility(spellob, 0, "")
 spellob.Remove()
 #Thank you Rednaxela.

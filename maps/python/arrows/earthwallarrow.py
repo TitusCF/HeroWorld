3d51cc923876dfc7690d57944a49a3d357f3b137
TitusCF <46248632+TitusCF@users.noreply.github.com>
2020-02-11 10:25:27 -0500

    earthwall arrow

---
+++ maps/python/arrows/earthwallarrow.py
@@ -0,0 +1,13 @@
+import Crossfire
+spellob = Crossfire.CreateObjectByName("spell_create_earth_wall")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+spellob = Crossfire.CreateObjectByName("spell_create_earth_wall")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+spellob = Crossfire.CreateObjectByName("spell_create_earth_wall")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+
+
+

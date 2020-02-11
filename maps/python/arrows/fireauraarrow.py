ed887bc706657d537b6606a477907902a0521263
TitusCF <46248632+TitusCF@users.noreply.github.com>
2020-02-11 09:59:11 -0500

    vitriol splash arrow

--- maps/python/arrows/fireauraarrow.py
+++ maps/python/arrows/fireauraarrow.py
@@ -1,7 +1,7 @@
 import Crossfire
 #Rednaxela helped me alot with this, gave me all the code --MikeeUSA--
 #Fire burning hands in all directions (splashback).
-spellob = Crossfire.CreateObjectByName("spell_vitriol")
+spellob = Crossfire.CreateObjectByName("spell_vitriol_splash")
 Crossfire.WhoAmI().CastAbility(spellob, 0, "")
 spellob.Remove()
 #Cast create bomb spell (Igniting the bomb at the end of the arrow)

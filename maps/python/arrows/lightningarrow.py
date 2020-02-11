3105c90da0e0008df39e41d61de90065a566334b
TitusCF <46248632+TitusCF@users.noreply.github.com>
2020-02-11 11:35:34 -0500

    new arrows to lake country bow shop

---
+++ maps/python/arrows/lightningarrow.py
@@ -0,0 +1,13 @@
+import Crossfire
+spellob = Crossfire.CreateObjectByName("spell_large_lightning")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+spellob = Crossfire.CreateObjectByName("spell_large_lightning")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+spellob = Crossfire.CreateObjectByName("spell_large_lightning")
+Crossfire.WhoAmI().CastAbility(spellob, 0, "")
+spellob.Remove()
+
+
+

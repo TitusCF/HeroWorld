d8d69d6e656954c06f87a66941e414dd81d556d2
TitusCF <46248632+TitusCF@users.noreply.github.com>
2020-03-06 17:19:27 -0500

    minor nerf

--- maps/python/arrows/vitriolarrow.py
+++ maps/python/arrows/vitriolarrow.py
@@ -2,12 +2,6 @@ import Crossfire
 spellob = Crossfire.CreateObjectByName("spell_vitriol_splash")
 Crossfire.WhoAmI().CastAbility(spellob, 0, "")
 spellob.Remove()
-spellob = Crossfire.CreateObjectByName("spell_vitriol_splash")
-Crossfire.WhoAmI().CastAbility(spellob, 0, "")
-spellob.Remove()
-spellob = Crossfire.CreateObjectByName("spell_vitriol_splash")
-Crossfire.WhoAmI().CastAbility(spellob, 0, "")
-spellob.Remove()
 
 
 
